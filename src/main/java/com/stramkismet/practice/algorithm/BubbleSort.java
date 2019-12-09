aff9fed05c9f41e68f3dab03284fa51c61397e9f
shangxi <hao.sun@expread.io>
2019-12-09 17:52:25 +0800

    增加冒泡算法

--- src/main/java/com/stramkismet/practice/algorithm/BubbleSort.java
+++ src/main/java/com/stramkismet/practice/algorithm/BubbleSort.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 排序算法
+ * 相邻的数据进行比较，如果a[i]>a[i+1]则两数交换位置
  *
  * @author shangxi
  */
@@ -15,6 +16,10 @@ public class BubbleSort {
         }
         int len = arr.length;
         for (int i = 0; i < len; i++) {
+            // len -i -1 :
+            //-1是为了防止数组越界即当数组到了len-1时会去出len-1和len的数据
+            //-i是为了减少比较次数,每遍循环完成后最大的数处于最后的位置，
+            //因此不需要继续向后遍历
             for (int j = 0; j < len - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j + 1];
